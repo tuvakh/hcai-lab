d3bd64ba5720b88107664b09d5c099987eaccc7f
tuvakh <tuvakh@stud.ntnu.no>
2026-03-24 15:53:27 +0100

    fixed mobile view

--- webpage/src/pages/Projects.jsx
+++ webpage/src/pages/Projects.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router";
 import HeroSection from "../components/HeroSection";
 import CardGrid from "../components/CardGrid";
 import { projects } from "../data/projectsData";
