0c7325f4cbfa0c0ee47744eab37ad53d5b9f16e1
Nic0Bu <ni_bu@live.com>
2026-04-21 15:21:14 +0200

    .

--- backend/routes/upload.js
+++ backend/routes/upload.js
@@ -3,15 +3,17 @@ const multer = require("multer");
 const path = require("path");
 const router = express.Router();
 
+const UPLOAD_DIR = path.join(__dirname, "../../webpage/public/assets/uploads");
+
 const storage = multer.diskStorage({
-  destination: path.join(__dirname, "../../webpage/public/assets/events"),
+  destination: (req, file, cb) => cb(null, UPLOAD_DIR),
   filename: (req, file, cb) => cb(null, Date.now() + "-" + file.originalname),
 });
 
 const upload = multer({ storage });
 
 router.post("/", upload.single("file"), (req, res) => {
-  res.json({ path: `/assets/events/${req.file.filename}` });
+  res.json({ path: `/assets/uploads/${req.file.filename}` });
 });
 
 module.exports = router;
