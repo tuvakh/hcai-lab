3a4138e114ff14ea3e1e92fadaddddb9313c3be7
tuvakh <tuvakh@stud.ntnu.no>
2026-05-05 12:11:27 +0200

    fixed the e2e and component tests

--- webpage/tests/reactComponent/setup.js
+++ webpage/tests/reactComponent/setup.js
@@ -1,8 +1,1 @@
 import '@testing-library/jest-dom';
-import { setupServer } from 'msw/node';
-
-export const server = setupServer();
-
-beforeAll(() => server.listen())
-afterEach(() => server.resetHandlers())
-afterAll(() => server.close())
